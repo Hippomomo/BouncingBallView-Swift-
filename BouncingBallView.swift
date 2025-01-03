0c770eb312847def94f7b3a805913ac74141a18b
Hippomomo <159291309+Hippomomo@users.noreply.github.com>
2025-01-03 16:21:58 +0800

    Update BouncingBallView.swift

--- BouncingBallView.swift
+++ BouncingBallView.swift
@@ -1,8 +1,5 @@
 //
 //  BouncingBallView.swift
-//  
-//
-//  
 //
 
 import SwiftUI
@@ -10,17 +7,22 @@ import SwiftUI
 struct BouncingBallView: View {
     @State private var offsetY: CGFloat = -300
     @State private var isBouncing = false
+    @State private var size: CGFloat = 50
+    @State private var gradientStart = UnitPoint.topLeading
+    @State private var gradientEnd = UnitPoint.bottomTrailing
 
     var body: some View {
         ZStack {
-            Color.black.ignoresSafeArea()
+            
+//            Color.black.ignoresSafeArea()
             
             Circle()
-                .fill(Color.orange)
-                .frame(width: 50, height: 50)
+                .fill(LinearGradient(colors: [Color.green, Color.blue, Color.purple],startPoint: gradientStart,endPoint: gradientEnd))
+                .frame(width: size, height: size)
                 .offset(y: offsetY)
                 .onAppear {
                     bounce()
+                    zoom()
                 }
         }
     }
@@ -30,6 +32,13 @@ struct BouncingBallView: View {
             offsetY = 300
         }
     }
+    
+    private func zoom() {
+        withAnimation(Animation.interpolatingSpring(stiffness: 200, damping: 5).repeatForever(autoreverses: false)) {
+            size = 500
+        }
+    }
+    
 }
 
 struct BouncingBallView_Previews: PreviewProvider {
